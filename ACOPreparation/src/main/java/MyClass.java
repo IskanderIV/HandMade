0eb540444c31c9dddb468b632e1ab648f316e442
Alexandr <19alex86@mail.ru>
2017-10-03 01:42:33 +0400

    References

--- ACOPreparation/src/main/java/MyClass.java
+++ ACOPreparation/src/main/java/MyClass.java
@@ -1,5 +1,7 @@
 import java.util.Date;
 
+import static java.lang.System.out;
+
 /**
  * Created by Alexandr on 23.09.2017.
  */
@@ -10,7 +12,7 @@ public class MyClass {
         println("in MyClass static field");
     }
 
-    public MyClass(){
+    public MyClass() {
         println("in MyClass constructor");
     }
 
@@ -26,6 +28,15 @@ public class MyClass {
         println(staticClass.field);
         changeObject(staticClass);
         println(staticClass.field);
+        NonStaticObject nonStaticObject1 = new NonStaticObject();
+        NonStaticObject nonStaticObject2 = new NonStaticObject();
+        nonStaticObject1.b = 5;
+        nonStaticObject2 = nonStaticObject1;
+        println(nonStaticObject2);
+        println(nonStaticObject1);
+        nonStaticObject1.b = 7;
+        println(nonStaticObject2);
+        println(nonStaticObject1);
     }
 
     static void changeString(String str) {
@@ -33,11 +44,14 @@ public class MyClass {
         println(str);
     }
 
-    static void println(String str){
-        System.out.println(str);
+    static void println(Object str) {
+        out.println(str);
+    }
+    static void print(Object str) {
+        out.print(str);
     }
 
-    static void changeObject(Object obj){
+    static void changeObject(Object obj) {
         obj = new Date();
         println(obj.toString());
     }
---
+++ ACOPreparation/src/main/java/NonStaticObject.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Alexandr on 03.10.2017.
+ */
+public class NonStaticObject {
+    private int k = 10;
+    public int b = 2;
+
+    @Override
+    public String toString() {
+        return "NonStaticObject{" +
+                "k=" + k +
+                ", b=" + b +
+                '}';
+    }
+}
