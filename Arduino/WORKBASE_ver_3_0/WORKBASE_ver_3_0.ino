ed9b4642612a556c8ae4773252727cb15d7527ab
Aleksandr <aleksandr_ivanov1@epam.com>
2017-10-20 01:56:23 +0400

    After merge with SMS_test where sms getting and setting is well. Connect with LCD - first stable version

--- Arduino/WORKBASE_ver_3_0/WORKBASE_ver_3_0.ino
+++ Arduino/WORKBASE_ver_3_0/WORKBASE_ver_3_0.ino
@@ -19,6 +19,21 @@
 #define SMS_LENGTH 160
 #define PHONE_LENGTH 20
 
+#define EMPTY_STRING "                "
+#define HELLO_STRING "HELLO"
+#define RADIO_INIT_STRING "RADIO INIT"
+#define GSM_POWERUP_STRING "GSM POWERUP"
+#define GSM_INIT_STRING "GSM INIT"
+#define CLEAN_SMS_MEMORY_STRING "CLEAN SMS MEMORY"
+#define RADIO_INIT_ERROR_STRING "RADIO INIT ERROR"
+#define GSM_INIT_ERROR_STRING "GSM INIT ERROR"
+#define MESSAGE_MEMORY_WAS_EMPTY_STRING "MEMORY WAS EMPTY" 
+#define MESSAGES_DELETED_STRING "MESSAGES DELETED" 
+
+#define SEARCH_DEVICES_STATE_STRING "SEARCH STATE:   "
+#define READY_FOR_SMS_STATE_STRING "READY FOR SMS:  "
+
+
 //#include <GSM.h>
 #include <sms.h>
 #include <EEPROM.h>
@@ -44,44 +59,82 @@ const int btn_Gsm_Power_Pin = 13;
 // screen
 LiquidCrystal_I2C lcd(0xbf, LCD_COL_NUM, LCD_ROW_NUM);
 
+// ERRORS
+boolean rf_error = false;
+boolean gsm_error = false;
+
 void setup() {
   Serial.begin(9600);
   lcd_init();
+  lcd.clear();
+  lcd_print_state(HELLO_STRING);
   digital_pin_init();
-<<<<<<< HEAD
-  radio_init();  
-  print_EEPROM();
-  gsm_power_on();
   delay(5000);
-  gsm_init();
-  delay(3000);
-=======
-  radio_init();
-  gsm_init();
+  lcd.clear();
+  lcd_print_state(RADIO_INIT_STRING);
+  rf_error = radio_init();
+  delay(5000);
+  if (!rf_error) {
+    lcd.clear();
+    lcd_print_state(GSM_POWERUP_STRING);
+    gsm_power_on();
+    delay(5000);
+    lcd.clear();
+    lcd_print_state(GSM_INIT_STRING);
+    gsm_error = gsm_init();
+    delay(3000);
 #ifdef MY_DEBUG
-  print_EEPROM();
+    print_EEPROM();
 #endif
-  clean_sms_memory();
-  delay(300);
->>>>>>> refs/remotes/origin/master
+    if (!gsm_error) {
+      lcd.clear();
+      lcd_print_state(CLEAN_SMS_MEMORY_STRING);
+      clean_sms_memory();
+      delay(5000);
+      lcd_print_info(EMPTY_STRING, 0);
+    } else {
+      lcd.clear();
+      lcd_print_state(GSM_INIT_ERROR_STRING);
+    }
+  } else {
+    lcd.clear();
+    lcd_print_state(RADIO_INIT_ERROR_STRING);
+  }
 }
 
 void clean_sms_memory() {
-  int position = 0;
+#ifdef MY_DEBUG
+  Serial.println("INSIDE of clean_sms_memory");
+#endif
+  char position;
+  boolean empty_sms_memory = true;
   // Delete all messages from modem memory
   while (position = findSMS(SMS_ALL)) {
-    int result_of_deleting = gsm_sms.DeleteSMS(position);
+    char result_of_deleting = gsm_sms.DeleteSMS((byte) position);
+    Serial.print("result_of_deleting = ");
+    Serial.println((int) result_of_deleting);
 #ifdef MY_DEBUG
-    Serial.print("MESSAGE on position ");
-    Serial.print(result_of_deleting);
-    if (result_of_deleting > 0) {
+    Serial.print("FOUND MESSAGE in position ");
+    Serial.print((byte) position);
+    if (result_of_deleting) {
       Serial.println(" was really DELETED");
+      empty_sms_memory = false;
     } else {
       Serial.println(" was NOT really DELETED");
     }
 #endif
   }
-  Serial.println("MESSAGES WAS DELETED");
+  if (empty_sms_memory) {
+    lcd_print_info(MESSAGE_MEMORY_WAS_EMPTY_STRING, 0);
+    Serial.println("MESSAGE MEMORY WAS EMPTY");
+  } else {
+    lcd_print_info(MESSAGES_DELETED_STRING, 0);
+    Serial.println("MESSAGES HAVE BEEN DELETED");
+  }
+  gsm.SendATCmdWaitResp("AT+CPMS=\"SM\",\"SM\",\"SM\"", 1000, 1000, "+CPMS:", 10);
+#ifdef MY_DEBUG
+  Serial.println("OUT from clean_sms_memory");
+#endif
 }
 
 void print_EEPROM() {
@@ -94,7 +147,7 @@ void print_EEPROM() {
   Serial.println();
 }
 
-//MESSAGE
+//RADIO MESSAGE DATA STRUCTURES
 const uint8_t msgLength = 1;
 //output data
 uint8_t data[msgLength];
@@ -104,98 +157,142 @@ uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
 uint8_t numDevices = 0;
 
 void loop() {
-  readButtons();
+  if (!rf_error && !gsm_error) {
+    Serial.println("-----------LOOP-------------");
+    readButtons();
 
-  // FIND STATE // works like a server get request send response
-  if (btn_Search_Remember) {
-    // works like a server. Waits messages from devices and retries ack
-    if (manager.available()) {
-      Serial.println("got something!!");//TEST
-      uint8_t from;
-      uint8_t len = sizeof(buf);
-      if (manager.recvfromAck(buf, &len, &from)) {
-        printMessageInSerial(buf, from);
-        uint8_t device_number = 0;
-        for (int i = 0; i < MAX_DEVICES; i++) {
-          if ((device_number = EEPROM.read(i)) == 0) {
-            Serial.println("No such device found");//TEST
-            device_number = i + 1;
-            Serial.print("BAZA: new_device_number = ");//TEST
-            Serial.println(device_number);//TEST
-            break;
-          } else if (device_number == from) {
-            Serial.println("device_number == from");//TEST
-            break;
+    // FIND DEVICES STATE // works like a server get request send response
+    if (btn_Search_Remember) {
+      // works like a server. Waits messages from devices and retries ack
+      lcd_print_state(SEARCH_DEVICES_STATE_STRING);
+      if (manager.available()) {
+        Serial.println("BAZA: got something by RF!!");//TEST
+        uint8_t from;
+        uint8_t len = sizeof(buf);
+        if (manager.recvfromAck(buf, &len, &from)) {
+#ifdef MY_DEBUG
+          printMessageInSerial(buf, from);
+#endif
+          uint8_t device_number = 0;
+          for (int i = 0; i < MAX_DEVICES; i++) {
+            if ((device_number = EEPROM.read(i)) == 0) {
+              Serial.println("BAZA: No such device found");//TEST
+              device_number = i + 1;
+              Serial.print("BAZA: new device number = ");//TEST
+              Serial.println(device_number);//TEST
+              break;
+            } else if (device_number == from) {
+              Serial.println("BAZA: device was identified");//TEST
+              break;
+            }
           }
-        }
 
-        prepareData(device_number);
-        print_RF_on_lcd(convertUint8ToString(data));
-
-        // Send a reply back to the originator client
-        serialPrint("BAZA: data = ", data[0]);//TEST
-        serialPrint("BAZA: from = ", from);//TEST
-        if (manager.sendtoWait(data, sizeof(data), from)) {
-          EEPROM.write(device_number - 1, device_number);
+          prepareData(device_number);
+          // Send a reply back to the originator client
+          serialPrint("BAZA: data = ", data[0]);//TEST
+          serialPrint("BAZA: from = ", from);//TEST
+          if (manager.sendtoWait(data, sizeof(data), from)) {
+            EEPROM.write(device_number - 1, device_number);
+          } else {
+            Serial.println("BAZA: sendtoWait failed");
+          }
+#ifdef MY_DEBUG
+          print_EEPROM();//TEST
+#endif
         } else {
-          Serial.println("sendtoWait failed");
+
         }
-        print_EEPROM();//TEST
-      } else {
-        print_RF_on_lcd("Bad");
       }
     }
-  }
-  // WORK STATE // works like a client: send request get response
-  else {
-    Serial.println("WORK STATE");
-    int position = findSMS(SMS_UNREAD);
-    if (position) {
-      Serial.println("----------====<<<<< GET SMS >>>>>====----------");
-
-      char phone[PHONE_LENGTH];
-      char in_sms_text[SMS_LENGTH];
-      char out_sms_text[SMS_LENGTH];
-      getSMS(position, phone, in_sms_text);
-      Serial.print("phone = ");
-      Serial.println(phone);
-      Serial.print("in_sms_text = ");
-      Serial.println(in_sms_text);
-      uint8_t device_states[MAX_DEVICES];
-      memset(device_states, MAX_DEVICES, 0);
-      int devicesCount = quiz_devices(device_states);
-      Serial.println("after quiz_devices");
-      int num_symbols_in_sms = prepare_out_sms_text(out_sms_text, device_states, devicesCount);
-      Serial.println("after prepare_out_sms_text");
-      Serial.print("text sms = ");
-      Serial.println(out_sms_text);
-      Serial.print("num of symbols in sms = ");
-      Serial.println(num_symbols_in_sms);
-
-      sendSMS(phone, out_sms_text);
-      Serial.println("----------====<<<<< DEVICES ARE CHECKED >>>>>====----------");
+    // WORK STATE // works like a client: send request get response
+    else {
+      lcd_print_state(READY_FOR_SMS_STATE_STRING);
+      Serial.println("WORK STATE");
+      char position;
+      findSMS(SMS_UNREAD);
+      position = findSMS(SMS_READ);
+#ifdef MY_DEBUG
+      Serial.print("position when SMS_UNREAD = ");
+      Serial.println((int)position);
+#endif
+      if (position) {
+        Serial.println("----------====<<<<< GET SMS >>>>>====----------");
+        char phone[PHONE_LENGTH];
+        char in_sms_text[SMS_LENGTH];
+        char out_sms_text[SMS_LENGTH];
+        getSMS(position, phone, in_sms_text);
+#ifdef MY_DEBUG
+        Serial.print("phone = ");
+        Serial.println(phone);
+        Serial.print("in_sms_text = ");
+        Serial.println(in_sms_text);
+#endif
+        uint8_t device_states[MAX_DEVICES];
+        memset(device_states, MAX_DEVICES, 0);
+        int devicesCount = quiz_devices(device_states);
+        Serial.println("after quiz_devices");
+        int num_symbols_in_sms = prepare_out_sms_text(out_sms_text, device_states, devicesCount);
+#ifdef MY_DEBUG
+        Serial.println("after prepare_out_sms_text");
+        Serial.print("text sms = ");
+        Serial.println(out_sms_text);
+        Serial.print("num of symbols in sms = ");
+        Serial.println(num_symbols_in_sms);
+#endif
+        sendSMS(phone, out_sms_text);
+        Serial.println("----------====<<<<< DEVICES ARE CHECKED >>>>>====----------");
+      }
+      delay(1500);
     }
-    delay(1000);
+  } else {
+    if (rf_error) {
+      Serial.println("Radio init error!");
+    }
+    if (gsm_error) {
+      Serial.println("GSM init error!");
+    }
+    delay(5000);
   }
 }
 
-int findSMS(byte required_status) {
+char findSMS(byte required_status) {
+#ifdef MY_DEBUG
+  Serial.println("INSIDE of findSMS");
+  Serial.print("findSMS type = ");
+  switch (required_status) {
+    case SMS_UNREAD:
+      Serial.println("AT+CMGL=\"REC UNREAD\"");
+      break;
+    case SMS_READ:
+      Serial.println("AT+CMGL=\"REC READ\"");
+      break;
+    case SMS_ALL:
+      Serial.println("AT+CMGL=\"ALL\"");
+      break;
+  }
+  Serial.println("OUT from findSMS");
+#endif
+
   return gsm_sms.IsSMSPresent(required_status);
 }
 
-void getSMS(int position, char *phone, char* sms_text) {
-  if (position > 0 && position <= 20) {
-    gsm_sms.GetSMS((byte)position, phone, sms_text, SMS_LENGTH);
-    Serial.println(phone);
-    // Delete message from modem memory
-    int result_delete = gsm_sms.DeleteSMS(position);
+void getSMS(char position, char *phone, char* sms_text) {
 #ifdef MY_DEBUG
-    if (result_delete > 0) {
-      Serial.println("MESSAGE WAS really DELETED");
-    }
+  Serial.println("INSIDE of getSMS");
 #endif
-    Serial.println("MESSAGE DELETED");
+  if (position > 0 && position <= 20) {
+#ifdef MY_DEBUG
+    gsm.SendATCmdWaitResp("AT+CPMS=\"SM\",\"SM\",\"SM\"", 1000, 1000, "+CPMS:", 10);
+#endif
+    gsm_sms.GetSMS((byte)position, phone, sms_text, SMS_LENGTH);
+#ifdef MY_DEBUG
+    gsm.SendATCmdWaitResp("AT+CPMS=\"SM\",\"SM\",\"SM\"", 1000, 1000, "+CPMS:", 10);
+#endif
+    clean_sms_memory();
   }
+#ifdef MY_DEBUG
+  Serial.println("OUT from getSMS");
+#endif
 }
 
 int quiz_devices(uint8_t *device_states) {
@@ -232,42 +329,37 @@ int quiz_devices(uint8_t *device_states) {
 }
 
 int prepare_out_sms_text(char* sms_text, uint8_t* device_states, int device_count) {
+  String str;
+
+#ifdef MY_DEBUG
+  Serial.println("INSIDE  prepare_out_sms_text() method");
+#endif
   if (device_count != 0) {
+    char number[3];
     int symbol_position = 0;
     for (int i = 0; i < device_count; i++) {
-      sms_text[symbol_position] = 'D';
-      symbol_position++;
-      char device_count_Char[3];
-      itoa(i, device_count_Char, 10);
-      for (int j = 0; j < 3; j++) {
-        sms_text[symbol_position] = device_count_Char[j];
-        symbol_position++;
-      }
-      sms_text[symbol_position] = '=';
-      symbol_position++;
-      char device_state_Char[3];
-      itoa(device_states[i], device_state_Char, 10);
-      for (int j = 0; j < 3; j++) {
-        sms_text[symbol_position] = device_state_Char[j];
-        symbol_position++;
-      }
-      sms_text[symbol_position] = '\n';
-      symbol_position++;
-      return symbol_position - 1;
+      str += 'D';
+      String device_number = String(i + 1, DEC);
+      str += device_number;
+      str += '=';
+      String device_state = String((int)device_states[i], DEC);
+      str += device_state;
+      str += ';';
     }
   } else {
-    String str = "Error 0: No devices found";
-    strcpy(sms_text, str.c_str());
-    return str.length();
+    str = "Error 0: No devices found";
   }
-
-
+  strcpy(sms_text, str.c_str());
+#ifdef MY_DEBUG
+  Serial.println("OUT from  prepare_out_sms_text() method");
+#endif
+  return str.length();
 }
 
 void sendSMS(char* phone, char* sms_text) {
-  Serial.println("SMS send has been started");
+  Serial.println("SMS sending has been started");
   if (gsm_sms.SendSMS(phone, sms_text)) {
-    Serial.println("SMS send has been finished");
+    Serial.println("SMS sending has been finished");
   } else {
     Serial.println("There is an ERROR while sending sms");
   }
@@ -288,36 +380,31 @@ String convertUint8ToString(uint8_t *data) {
 void lcd_init() {
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(1, LCD_ROW_1);
-  lcd.print("Hello, My Lord!");
 }
 
 void digital_pin_init() {
   pinMode(btn_Search_Remember_Pin, INPUT);
-//  pinMode(btn_get_SMS_Pin, INPUT);
-  //  pinMode(pwr_Pin, OUTPUT);
-  //  digitalWrite(pwr_Pin, true);
 }
 
-void radio_init() {
+boolean radio_init() {
   lcd.setCursor(LCD_RF_STATE_COL, LCD_ROW_2);
-  if (!manager.init()) {
-    lcd.print("RF=0");
-    return;
+  if (manager.init()) {
+    return false;
   }
-  lcd.print("RF=1");
+  return true;
 }
 
 void readButtons() {
   btn_Search_Remember = digitalRead(btn_Search_Remember_Pin);
-  // btn_get_SMS = digitalRead(btn_get_SMS_Pin);
 }
 
-void print_RF_on_lcd(String str) {
-  if (str.length() > 3) {
-    str = str.substring(0, 2);
-  }
-  lcd.setCursor(LCD_RF_DATA_COL, LCD_ROW_2);
+void lcd_print_state(String str) {
+  lcd.setCursor(0, LCD_ROW_1);
+  lcd.print(str);
+}
+
+void lcd_print_info(String str, uint8_t col) {
+  lcd.setCursor(col, LCD_ROW_2);
   lcd.print(str);
 }
 
@@ -359,25 +446,27 @@ void prepareData(uint8_t device_number) {
 
 void gsm_power_on()
 {
- pinMode(btn_Gsm_Power_Pin, OUTPUT); 
- digitalWrite(btn_Gsm_Power_Pin,LOW);
- delay(1000);
- digitalWrite(btn_Gsm_Power_Pin,HIGH);
- delay(2000);
- digitalWrite(btn_Gsm_Power_Pin,LOW);
- delay(3000);
+#ifdef MY_DEBUG
+  Serial.println("INSIDE of gsm_power_on");
+#endif
+  pinMode(btn_Gsm_Power_Pin, OUTPUT);
+  digitalWrite(btn_Gsm_Power_Pin, LOW);
+  delay(1000);
+  digitalWrite(btn_Gsm_Power_Pin, HIGH);
+  delay(2000);
+  digitalWrite(btn_Gsm_Power_Pin, LOW);
+  delay(3000);
+#ifdef MY_DEBUG
+  Serial.println("OUT from gsm_power_on");
+#endif
 }
 
-void gsm_init() {
-  boolean notConnected = true;
-  while (notConnected) {
-    Serial.println("SMS Messages Sender");
-    if (gsm.begin(GSM_RATE)) {
-      notConnected = false;
-    } else {
-      Serial.println("Not connected");
-      delay(1000);
-    }
+boolean gsm_init() {
+  Serial.println("Start of GSM initialisation");
+  if (gsm.begin(GSM_RATE)) {
+    Serial.println("GSM is initialized");
+    return false;
   }
-  Serial.println("GSM initialized");
+  Serial.println("GSM is not initialized");
+  return true;
 }
