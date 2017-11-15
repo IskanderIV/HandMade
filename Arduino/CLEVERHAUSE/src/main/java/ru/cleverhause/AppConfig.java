bb03e91e432fe305cdc94d47f42bb241a3456d91
Alexandr <19alex86@mail.ru>
2017-11-16 01:42:40 +0400

    Initial configurations for maven

---
+++ Arduino/CLEVERHAUSE/src/main/java/ru/cleverhause/AppConfig.java
@@ -0,0 +1,7 @@
+package ru.cleverhause;
+
+/**
+ * Created by Alexandr on 15.11.2017.
+ */
+public class AppConfig extends Context{
+}
---
+++ Arduino/CLEVERHAUSE/src/main/java/ru/cleverhause/rest/MobileRestAPI.java
@@ -0,0 +1,7 @@
+package ru.cleverhause.rest;
+
+/**
+ * Created by Alexandr on 15.11.2017.
+ */
+public class MobileRestAPI {
+}
